8f98198da95ee498e962835e507b0e70813a2740
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-28 15:42:42 -0400

    Create a navbar using bootstrap

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,11 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import Navigation from "./Navigation"
+
 import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Learn React</header>
-    </div>
-  )
+  return <Navigation />
 }
 
 export default App
---
+++ my-app/src/components/Navigation.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+
+function Navigation() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Piotr Coffee Co.</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#coffee">Coffee</Nav.Link>
+            <Nav.Link href="#equipment">Equipment</Nav.Link>
+            <Nav.Link href="#apparel">Apparel</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navigation
