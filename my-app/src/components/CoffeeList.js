b276c5dac623fb9e36c5181829acff1212aa73b9
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-30 23:10:06 -0400

    Basic form on Coffee tab, no POST yet

--- my-app/src/components/CoffeeList.js
+++ my-app/src/components/CoffeeList.js
@@ -2,6 +2,7 @@ import React from "react"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import AddNewCoffee from "./AddNewCoffee"
 import CoffeeCard from "./CoffeeCard"
 
 import "../App.css"
@@ -12,6 +13,7 @@ function CoffeeList({ coffeeListings }) {
       <div>
         <body>
           <h2>Coffee</h2>
+          <AddNewCoffee />
           <Container fluid>
             <Row>
               {coffeeListings.map((coffeeListing) => (
