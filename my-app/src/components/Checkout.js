9f740bf9f3ea416a0bde6695f00a55b08d327ab0
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-03 16:02:57 -0400

    Refactor checkout

--- my-app/src/components/Checkout.js
+++ my-app/src/components/Checkout.js
@@ -3,19 +3,21 @@ import { Button } from "react-bootstrap"
 import Row from "react-bootstrap/Row"
 
 function Checkout({ cartItems, updateQty }) {
-  const [currentCoffeeListings, setCurrentCoffeeListings] = useState([])
-  const [currentEquipmentListings, setCurrentEquipmentListings] = useState([])
+  const [coffeeListings, setCoffeeListings] = useState([])
+  const [equipmentListings, setEquipmentListings] = useState([])
+
+  const allListings = [...coffeeListings, ...equipmentListings]
 
   const fetchCoffeeListings = () => {
     fetch("http://localhost:3001/coffee")
       .then((response) => response.json())
-      .then((data) => setCurrentCoffeeListings(data))
+      .then((data) => setCoffeeListings(data))
   }
 
   const fetchEquipmentListings = () => {
     fetch("http://localhost:3001/equipment")
       .then((response) => response.json())
-      .then((data) => setCurrentEquipmentListings(data))
+      .then((data) => setEquipmentListings(data))
   }
 
   useEffect(() => {
@@ -25,33 +27,15 @@ function Checkout({ cartItems, updateQty }) {
 
   const handleClick = () => {
     cartItems.forEach((cartItem) => {
-      currentCoffeeListings.forEach((coffeeListing) => {
-        if (coffeeListing.product_id === cartItem.product_id) {
-          // console.log("This is the coffee item listing: ", coffeeListing)
-
-          fetch(`http://localhost:3001/coffee/${coffeeListing.id}`, {
+      allListings.forEach((listing) => {
+        if (listing.product_id === cartItem.product_id) {
+          fetch(`http://localhost:3001/${listing.product_type}/${listing.id}`, {
             method: "PATCH",
             headers: {
               "Content-Type": "application/json",
             },
             body: JSON.stringify({
-              in_stock_qty: coffeeListing.in_stock_qty - 1,
-            }),
-          }).then(updateQty)
-        }
-      })
-
-      currentEquipmentListings.forEach((equipmentListing) => {
-        if (equipmentListing.product_id === cartItem.product_id) {
-          // console.log("This is the equipment item listing: ", equipmentListing)
-
-          fetch(`http://localhost:3001/equipment/${equipmentListing.id}`, {
-            method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              in_stock_qty: equipmentListing.in_stock_qty - 1,
+              in_stock_qty: listing.in_stock_qty - 1,
             }),
           }).then(updateQty)
         }
